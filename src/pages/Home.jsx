563e92bb41672d2721f08e5c58a82a86e61f0e1b
Pels <pelumisunday@gmail.com>
2023-08-22 15:10:02 +0100

    update: use CustomButton in Home comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import {
 } from '../config/motion'
 
 import state from '../store'
+import { CustomButton } from '../components'
 
 const Home = () => {
   const snap = useSnapshot(state)
@@ -27,6 +28,21 @@ const Home = () => {
               <h1 className='head-text'>
                 LET&apos;S <br className='xl:block hidden' /> DO IT.
               </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className='flex flex-col gap-5'>
+              <p className='max-w-md font-normal text-gray-600 text-base'>
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tool. <strong>Unleash your imagination</strong>{' '}
+                and define your own style.
+              </p>
+              <CustomButton
+                type='filled'
+                title='Customize It'
+                handleClick={() => (state.intro = false)}
+                customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+              />
             </motion.div>
           </motion.div>
         </motion.section>
