a5deb64db5843ea65bc044d2d2dc6d2d93d6a177
Pels <pelumisunday@gmail.com>
2023-08-22 11:20:23 +0100

    update: add heading texts with animations

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,39 @@
-import { motion, AnimatePresence } from "framer-motion"
-import { useSnapshot } from "valtio"
-import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from "../config/motion"
+import { motion, AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from '../config/motion'
+
+import state from '../store'
 
 const Home = () => {
+  const snap = useSnapshot(state)
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className='home' {...slideAnimation('left')}>
+          <motion.header {...slideAnimation('down')}>
+            <img
+              src='./threejs.png'
+              alt='logo'
+              className='w-8 h-8 object-contain'
+            />
+          </motion.header>
+          <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                LET&apos;S <br className='xl:block hidden' /> DO IT.
+              </h1>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
 export default Home
+
