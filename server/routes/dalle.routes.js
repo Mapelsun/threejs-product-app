521951168a823cbd70c8ed4d8537894f98dd71b6
Pels <pelumisunday@gmail.com>
2023-08-24 18:12:00 +0100

    feat: call backend to generate ai image

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -10,19 +10,25 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: 'user', content: 'Say this is a test' }],
-    model: 'gpt-3.5-turbo',
-  });
-
-  console.log(completion.choices);
-}
-
-main()
-
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL.E ROUTES' })
+})
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body
+    const response = await openai.images.generate({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    })
+    const image = response.data.data[0].b64_json
+    res.status(200).json({ photo: image })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
 })
 
 export default router
